bbd3d8079aa216db116b2a4d2f722646ad1f24be
MarianoMerakiDev <batistamariano479@gmail.com>
2025-04-21 17:28:44 -0300

    aca tengo que explicar que hice

---
+++ src/marianomerakidev/marianete.jsx
@@ -0,0 +1,13 @@
+import '../App.css'
+
+function marianete() {
+
+    return (
+        <>
+            <h2></h2>Marianete anduvo aqui
+            <div></div>
+        </>
+    )
+}
+
+export default marianomerakidev
