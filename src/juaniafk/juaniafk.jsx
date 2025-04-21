ca7750633c83b609024189e8f3293ae5619d4b27
juani-afk <juanmartinbalegre@gmail.com>
2025-04-21 17:36:11 -0300

    BUENA

---
+++ src/juaniafk/juaniafk.jsx
@@ -0,0 +1,22 @@
+import '../App.css'
+
+function juaniafk() {
+
+  return (
+    <>
+      <h2>A</h2>
+      <div>
+        <p>bb</p>
+      </div>
+
+
+
+
+
+
+
+    </>
+  )
+}
+
+export default juaniafk
