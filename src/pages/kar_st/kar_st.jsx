74ebbcc381144dfdc5c50acd923a892e85b79284
kar-st <karenfern44@gmail.com>
2025-08-05 17:03:47 -0300

    Agregado de Karen

--- src/pages/kar_st/kar_st.jsx
+++ src/pages/kar_st/kar_st.jsx
@@ -1,11 +1,12 @@
 import '../../index.css'
 
-function Kar_st() {
-  
+  import { Link } from 'react-router-dom'
+  function Kar_st() {
   return (
     <>
       <h1>Página de Karen</h1>
-      
+      <h2>Se añadirá en algún momento.</h2>
+      <Link className="button" to="/">Volver al inicio</Link>
     </>
   )
 }
